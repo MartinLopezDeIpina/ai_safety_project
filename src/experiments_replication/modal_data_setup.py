ada0d6977cb8c57fabcc72dbb032f5d2060f46fb
martin <martinsaski@gmail.com>
2026-07-09 13:08:48 +0200

    remove previously generated scripts

--- src/experiments_replication/modal_data_setup.py
+++
@@ -1,6 +0,0 @@
-import modal
-
-data_vol = modal.Volume.from_name("behavior-datasets", create_if_missing=True)
-
-with data_vol.batch_upload(force=True) as batch:
-    batch.put_directory("/home/martin/LLMs_Encode_Harmfulness_Refusal_Separately/data", "/")
